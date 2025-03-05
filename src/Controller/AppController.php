922c3cc0e32c94ca8c47abbdf934ea859597ac82
Grototo <th.guille@hotmail.com>
2025-03-05 16:30:45 +0100

    templating

---
+++ src/Controller/AppController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AppController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function appAbout(): Response
+    {
+        return $this->render('app/about.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/products', name: 'app_products')]
+    public function appProducts(): Response
+    {
+        return $this->render('app/products.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/why', name: 'app_why')]
+    public function appWhy(): Response
+    {
+        return $this->render('app/why.html.twig', [
+            
+        ]);
+    }
+
+    #[Route('/testimonials', name: 'app_testimonials')]
+    public function appTestimonials(): Response
+    {
+        return $this->render('app/testimonials.html.twig', [
+            
+        ]);
+    }
+}
