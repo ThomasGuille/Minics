f4b93bc3d233a584fc7bc5b63f696a28b1d6aea3
Grototo <th.guille@hotmail.com>
2025-03-06 08:29:03 +0100

    templating

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -11,40 +11,30 @@ final class AppController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('app/index.html.twig', [
-            
-        ]);
+        return $this->render('app/index.html.twig');
     }
 
     #[Route('/about', name: 'app_about')]
     public function appAbout(): Response
     {
-        return $this->render('app/about.html.twig', [
-            
-        ]);
+        return $this->render('app/about.html.twig');
     }
 
     #[Route('/products', name: 'app_products')]
     public function appProducts(): Response
     {
-        return $this->render('app/products.html.twig', [
-            
-        ]);
+        return $this->render('app/products.html.twig');
     }
 
     #[Route('/why', name: 'app_why')]
     public function appWhy(): Response
     {
-        return $this->render('app/why.html.twig', [
-            
-        ]);
+        return $this->render('app/why.html.twig');
     }
 
     #[Route('/testimonials', name: 'app_testimonials')]
     public function appTestimonials(): Response
     {
-        return $this->render('app/testimonials.html.twig', [
-            
-        ]);
+        return $this->render('app/testimonials.html.twig');
     }
 }
