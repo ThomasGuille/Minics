0fdfae9a23b64c2906cf848b034d234d7bb46a51
Grototo <th.guille@hotmail.com>
2025-03-07 09:59:10 +0100

    templates back/front - register/login

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             // dump($passwordHash);
             // dump($user);
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
 
 
