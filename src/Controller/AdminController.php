ef0a00765761cb657695df18112148137a79bcca
Grototo <th.guille@hotmail.com>
2025-03-10 16:52:52 +0100

    backoffice

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 final class AdminController extends AbstractController
 {
@@ -33,8 +39,79 @@ final class AdminController extends AbstractController
     }
 
     #[Route('/admin/category', name: 'app_admin_category')]
-    public function adminCategory(): Response
+    public function adminCategory(Request $request, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): Response
     {
-        return $this->render('admin/category.html.twig');
+        $category = new Category;
+        $form = $this->createForm(CategoryFormType::class, $category);
+
+        // $category->setTitle($_POST['title])
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $category->setCreatedAt(new \DateTimeImmutable());
+
+            // stmt->prepare(INSERT INTO category VALUES (:title))
+            // stmt->bindValue(":title", $category->getTitle(), PDO::PARAM_STR)
+            $entityManager->persist($category);
+
+            // stmt->execute()
+            $entityManager->flush();
+
+            $this->addFlash("success", "La catégorie a été enregistrée.");
+            return $this->redirectToRoute("app_admin_category");
+        }
+
+        /*
+        $data = $dbConnect->query(SELECT * FROM category);
+        $dbCategory = $data->fetchAll(PDO::FETCH_ASSOC);
+
+        La classe Repository contient des méthodes permettant uniquement d'exécuter des requêtes de sélection en BDD
+            find($id), findAll(), findBy(), findOneBy()
+        */
+
+        $dbCategory = $repoCategory->findAll();
+        // dump($dbCategory);
+
+        return $this->render('admin/category.html.twig', [
+            "categoryForm" => $form,
+            "dbCategory" => $dbCategory
+        ]);
+    }
+
+    #[Route('/admin/category/update/{id}', name: 'app_admin_category_update')]
+    public function adminCategoryUpdate($id, Category $category, Request $request, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): Response {
+        // dump($category);
+        // dump($id);
+
+        // SELECT * FROM category WHERE id = $id
+        // + fetch(PDO::FETCH_ASSOC);
+        $category = $repoCategory->find($id);
+        // dump($category);
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            // UPDATE category SET title = $category->getTitle, description = $category->getDescription() WHERE id = $id;
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            dump($category->getTitle());
+            $categoryTitle = $category->getTitle();
+
+            $this->addFlash("success", "La catégorie <strong class='text-white'>$categoryTitle</strong> a été modifiée.");
+
+            return $this->redirectToRoute('app_admin_category');
+        }
+
+        $dbCategory = $repoCategory->findAll();
+
+        return $this->render("admin/category.html.twig", [
+            'categoryForm' => $form,
+            'dbCategory' => $dbCategory
+        ]);
+    }
+
+    #[Route('/admin/category/delete/{id}', name: 'app_admin_category_delete')]
+    public function adminCategoryDelete($id, EntityManagerInterface $entityManager, CategoryRepository $repoCategory) {
+
     }
 }
---
+++ src/Form/CategoryFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CategoryFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                "label" => "Titre de la catégorie",
+                "required" => false,
+                "constraints" => [
+                    new NotBlank([
+                        "message" => "Veuillez saisir un nom de catégorie"
+                    ])
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                "label" => "Description de la catégorie",
+                "required" => false,
+                "constraints" => [
+                    new NotBlank([
+                        "message" => "Veuillez saisir une description"
+                    ])
+                ],
+                "attr" => [
+                    "rows" => 10
+                ]
+            ])
+            // ->add('createdAt', null, [
+            //     'widget' => 'single_text',
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
